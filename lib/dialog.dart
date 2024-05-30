0f1bf6378041d26113d0c115981aa9b43c389a45
Samuel Makoholi <samuelmakoh@gmail.com>
2024-05-30 11:48:56 +0200

    dialog

---
+++ lib/dialog.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+
+class Dialogg extends StatelessWidget {
+  @override
+
+  const Dialogg({super.key});
+
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+
+      title: "Dialog",
+      home: Scaffold(
+        appBar: AppBar(title: Text("Dialog Title"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: [
+              ElevatedButton(onPressed: (){
+                //Get.defaultDialog();
+                Get.defaultDialog(
+                  title: "Dialog Title",
+                  titleStyle: TextStyle(fontSize: 25, color: Colors.white),
+                  middleText: "This is middle text",
+                  middleTextStyle: TextStyle(fontSize: 20, color: Colors.white),
+                  backgroundColor: Colors.blue,
+                  radius: 80,
+
+                  //to customize the middle text
+                  content: Row(
+                    children: [
+                      CircularProgressIndicator(),
+                      SizedBox(
+                        width: 16,
+                      ),
+
+                      Expanded(child: Text("Data loading"),
+                      )
+                    ],
+                  ),
+
+                  //Default cancel and confirm action
+
+                  textCancel: "Cancel",
+                  cancelTextColor: Colors.white,
+                  textConfirm: "Confirm",
+                  confirmTextColor: Colors.white,
+                  onCancel: () {
+                    
+                  },
+
+                  onConfirm: () {
+                    
+                  },
+
+                  //color for default cancel and confirm
+
+                  buttonColor: Colors.green,
+                );
+              }, 
+              child: Text("Show Dialog"))
+            ],
+          ),
+        ),
+        ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx/dialog.dart';
 import 'package:flutter_getx/navigationn.dart';
 import 'package:get/get.dart';
 import 'package:flutter_getx/snack_bar.dart';
 
 void main() {
-  runApp(const Navigationn());
+  runApp(const Dialogg());
 }
 
